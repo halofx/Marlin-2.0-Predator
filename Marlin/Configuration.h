5e0c958dddedb867ef66665ce21508a931eca2cb
Scott W <scott@halofx.com>
2020-04-23 03:36:35 -0500

    Dial back XY_Probe Speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1071,7 +1071,7 @@
 #define MIN_PROBE_EDGE 5
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED 4000
+#define XY_PROBE_SPEED 2000
 
 // Feedrate (mm/m) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
