812262247ca0e8315e44649ec859cfa578ea9f0d
Scott W <scott@halofx.com>
2020-11-30 01:54:10 -0600

    Bump extruder current to 900 for BMG.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2341,7 +2341,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      900
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
