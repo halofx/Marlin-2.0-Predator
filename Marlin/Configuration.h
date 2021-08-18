e5c4a8c93b636d034e2ae2ba850a59b1ca967e9d
HaloFX <scott@halofx.com>
2021-08-17 19:13:26 -0500

    R6 release

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -133,7 +133,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "PREDATOR r6 b05"
+#define CUSTOM_MACHINE_NAME "PREDATOR R6"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
