785cc6797731c9ccdf6aad3342e4dfba581b16ec
Scott W <scott@halofx.com>
2022-01-13 03:59:46 -0600

    Frivolous Games!

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1717,8 +1717,8 @@
   //#define BOOT_MARLIN_LOGO_ANIMATED // Animated Marlin logo. Costs ~3260 (or ~940) bytes of PROGMEM.
 
   // Frivolous Game Options
-  //#define MARLIN_BRICKOUT
-  //#define MARLIN_INVADERS
+  #define MARLIN_BRICKOUT
+  #define MARLIN_INVADERS
   //#define MARLIN_SNAKE
   //#define GAMES_EASTER_EGG          // Add extra blank lines above the "Games" sub-menu
 
