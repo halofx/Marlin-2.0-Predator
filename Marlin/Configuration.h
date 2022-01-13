91f46bd8f9358d47876a76770d3a487fbaba51de
Scott W <scott@halofx.com>
2022-01-13 03:00:25 -0600

    Delta settings, and Endstops.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -769,6 +769,68 @@
 #endif
 
 //===========================================================================
+//============================== Delta Settings =============================
+//===========================================================================
+// Enable DELTA kinematics and most of the default configuration for Deltas
+#define DELTA
+
+#if ENABLED(DELTA)
+
+// Make delta curves from many straight lines (linear interpolation).
+// This is a trade-off between visible corners (not enough segments)
+// and processor overload (too many expensive sqrt calls).
+#define DELTA_SEGMENTS_PER_SECOND 200
+
+// After homing move down to a height where XY movement is unconstrained
+//#define DELTA_HOME_TO_SAFE_ZONE
+
+// Delta calibration menu
+// uncomment to add three points calibration menu option.
+// See http://minow.blogspot.com/index.html#4918805519571907051
+#define DELTA_CALIBRATION_MENU
+
+// uncomment to add G33 Delta Auto-Calibration (Enable EEPROM_SETTINGS to store results)
+#define DELTA_AUTO_CALIBRATION
+
+// NOTE NB all values for DELTA_* values MUST be floating point, so always have a decimal point in them
+
+#if ENABLED(DELTA_AUTO_CALIBRATION)
+// set the default number of probe points : n*n (1 -> 7)
+#define DELTA_CALIBRATION_DEFAULT_POINTS 7
+#endif
+
+#if EITHER(DELTA_AUTO_CALIBRATION, DELTA_CALIBRATION_MENU)
+// Set the steprate for papertest probing
+#define PROBE_MANUALLY_STEP 0.05   // (mm)
+#endif
+
+// Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
+#define DELTA_PRINTABLE_RADIUS 185.0 // (mm)
+
+// Center-to-center distance of the holes in the diagonal push rods.
+#define DELTA_DIAGONAL_ROD 448.67 // (mm) Stock is 440.0, My custom arms are 440.45 + 24 for the magballs.
+
+// Distance between bed and nozzle Z home position
+#define DELTA_HEIGHT 417.82 // (mm) Get this value from G33 auto calibrate
+
+#define DELTA_ENDSTOP_ADJ  { -0.22, 0.00, -0.09 } // Get these values from G33 auto calibrate
+
+// Horizontal distance bridged by diagonal push rods when effector is centered.
+#define DELTA_RADIUS 227.80   // (mm) Get this value from G33 auto calibrate
+
+// Trim adjustments for individual towers
+// tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
+// measured in degrees anticlockwise looking from above the printer
+#define DELTA_TOWER_ANGLE_TRIM  { +0.21, -0.14, -0.07 }  // Get these values from G33 auto calibrate
+
+// Delta radius and diagonal rod adjustments (mm)
+//#define DELTA_RADIUS_TRIM_TOWER { 0.0, 0.0, 0.0 }
+//#define DELTA_DIAGONAL_ROD_TRIM_TOWER { 0.0, 0.0, 0.0 }
+
+#endif
+
+
+//===========================================================================
 //============================== Endstop Settings ===========================
 //===========================================================================
 
