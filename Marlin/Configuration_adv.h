9b975221d8711a6fd6673fd2795e1988bb0a1657
Scott W <scott@halofx.com>
2022-01-13 03:58:17 -0600

    Buffer settings

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2246,11 +2246,11 @@
 // The number of linear moves that can be in the planner at once.
 // The value of BLOCK_BUFFER_SIZE must be a power of 2 (e.g., 8, 16, 32)
 #if BOTH(SDSUPPORT, DIRECT_STEPPING)
-  #define BLOCK_BUFFER_SIZE  8
+  #define BLOCK_BUFFER_SIZE  16
 #elif ENABLED(SDSUPPORT)
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE  64
 #else
-  #define BLOCK_BUFFER_SIZE 16
+  #define BLOCK_BUFFER_SIZE  64
 #endif
 
 // @section serial
