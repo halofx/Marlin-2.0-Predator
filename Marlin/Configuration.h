24fa6807d1194add0f5f4ff3fbf45dcc7fd5a1b9
Scott W <scott@halofx.com>
2022-01-13 03:01:13 -0600

    Forgot Endstops settings.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -839,15 +839,15 @@
 // Specify here all the endstop connectors that are connected to any endstop or probe.
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
-#define USE_XMIN_PLUG
-#define USE_YMIN_PLUG
-#define USE_ZMIN_PLUG
+//#define USE_XMIN_PLUG
+//#define USE_YMIN_PLUG
+#define USE_ZMIN_PLUG     // Probe at bottom.
 //#define USE_IMIN_PLUG
 //#define USE_JMIN_PLUG
 //#define USE_KMIN_PLUG
-//#define USE_XMAX_PLUG
-//#define USE_YMAX_PLUG
-//#define USE_ZMAX_PLUG
+#define USE_XMAX_PLUG     // Home at top.
+#define USE_YMAX_PLUG     // Home at top.
+#define USE_ZMAX_PLUG     // Home at top.
 //#define USE_IMAX_PLUG
 //#define USE_JMAX_PLUG
 //#define USE_KMAX_PLUG
